fb290ca18d361cc71aff270301de0c0cf2596c13
Leevesh Kamboj <leeveshk12@gmail.com>
2022-01-21 21:47:37 +0530

    test

--- routes/index.js
+++ routes/index.js
@@ -75,6 +75,7 @@ router.get("/author", async (req, res) => {
   const body = await fetch(
     `http://ip-api.com/json/${ip}?fields=status,countryCode`
   ).then((res) => res.json());
+  console.log(body);
   if (body.success && body.countryCode === "IN") {
     return res.status(200).redirect("https://www.linkedin.com/in/cvkamboj/");
   } else {
